e37b9f773d07e4e3ed0ac76414a49dd9a3e97e41
Jonathon Douglas <jdouglas@acquisinsurance.com>
2015-10-22 16:44:53 +0100

    Fix proto generator script

--- generate_protos.cmd
+++ generate_protos.cmd
@@ -5,10 +5,10 @@
 @rem enter this directory
 cd /d %~dp0
 
-packages\Google.Protobuf.3.0.0-alpha4\tools\protoc.exe ^
+packages\Google.Protobuf\tools\protoc.exe ^
  -I./Greeter/protos ^
- --csharp_out . ^
- --grpc_out . ^
- --plugin=protoc-gen-grpc=packages\Grpc.Tools.0.7.1\tools\grpc_csharp_plugin.exe ./Greeter/protos/helloworld.proto
+ --csharp_out Greeter ^
+ --grpc_out Greeter ^
+ --plugin=protoc-gen-grpc=packages\Grpc.Tools\tools\grpc_csharp_plugin.exe ./Greeter/protos/helloworld.proto
 
 @endlocal
