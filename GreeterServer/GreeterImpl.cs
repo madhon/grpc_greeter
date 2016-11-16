3e29c0b61071432749c57c138fc31e2e341bfe56
Jonathon Douglas <jdouglas@acquisinsurance.com>
2016-11-16 15:19:20 +0000

    Minor tidy

--- GreeterServer/GreeterImpl.cs
+++ GreeterServer/GreeterImpl.cs
@@ -8,8 +8,6 @@
     public class GreeterImpl : Greeter.GreeterBase
     {
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-        {
-            return Task.FromResult(new HelloReply { Message = "Hello " + request.Name });
-        }
+            => Task.FromResult(new HelloReply { Message = "Hello " + request.Name });
     }
 }
